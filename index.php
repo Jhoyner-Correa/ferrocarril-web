c306a6f15c2e552b283e1d843c295722adc0bd03
Jhoyner Correa <correahinostrozajhoyner@gmail.com>
2026-03-10 00:14:16 -0500

    Mejora visual del panel web

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Clientes - Ferrocarril</title>
+    <title>CRUD-CLIENTES</title>
     <style>
         *{
             box-sizing: border-box;
